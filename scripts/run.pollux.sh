2faf29dcdb44454f296ca079dab0444edfe0cf79
Serghei Mangul <serghei@login2.(none)>
2017-08-23 02:54:58 -0700

    change template to run a tool

--- scripts/run.pollux.sh
+++ scripts/run.pollux.sh
@@ -2,6 +2,8 @@
 
 AUTHOR="imandric1"
 
+
+
 ################################################################
 ##########          The main template script          ##########
 ################################################################
@@ -26,13 +28,34 @@ compressScript="/u/home/i/imandric/project-zarlab/ErrorCorrection/scripts/compre
 # <---------------------------> <------------>
 
 
+
+
+if [ $# -lt 5 ]
+then
+echo "********************************************************************"
+echo "Script was written for project : Best practices for  conducting benchmarking in the most comprehensive and reproducible way"
+echo "This script was written by Igor Mandric"
+echo "********************************************************************"
+echo ""
+echo "1 <input1>  - _1.fastq "
+echo "2 <input2> - _2.fastq"
+echo "3 <tmpdir - dir to save the intermediate output"
+echo "4 <outdir - dir to save the output"
+echo "5 <kmer> - kmer length"
+echo "--------------------------------------"
+exit 1
+fi
+
+
+
 # mandatory part
 input1=$1
 input2=$2
-outdir=$3
+tmpdir=$3
+outdir=$4
 
 # extra part (tool specific)
-kmer=$4
+kmer=$5
 
 
 
@@ -49,7 +72,7 @@ kmer=$4
 
 # STEP 2 - run the tool (ATTENTION: TOOL SPECIFIC PART!)
 
-wdir=/tmp/$toolName-$( date +%Y-%m-%d-%H-%M-%S )
+wdir=${tmpdir}/$toolName-$( date +%Y-%m-%d-%H-%M-%S )
 
 #run the command (in a temporary directory)
 $toolPath -i $input1 $input2 -p -o $wdir -k $kmer
@@ -61,7 +84,7 @@ $toolPath -i $input1 $input2 -p -o $wdir -k $kmer
 
 # STEP 3 - transform output if necessary (ATTENTION: TOOL SPECIFIC PART!)
 
-cat $wdir/input_1.fastq.corrected $wdir/input_2.fastq.corrected > $wdir/${toolName}.corrected.fastq
+cat $wdir/input_1.fastq.corrected $wdir/input_2.fastq.corrected | gunzip - > $wdir/${toolName}.corrected.fastq.gz
 rm $wdir/input_1.fastq.corrected
 rm $wdir/input_2.fastq.corrected
 
@@ -71,27 +94,15 @@ rm $wdir/input_2.fastq.corrected
 
 
 
-# STEP 4 - compress output
+# STEP 4 - send the output to the designated directory
 
-python $compressScript $input1 $input2 $wdir/${toolName}.corrected.fastq $wdir/${toolName}.corrected.compressed.fastq
-
-# ------------------------
-
-
-
-
-
-# STEP 5 - send the output to the designated directory
-
-cp $wdir/${toolName}.corrected.compressed.fastq $outdir
+cp $wdir/${toolName}.corrected.compressed.fastq.gz $outdir
 
 # ----------------------------------------------------
 
 
 
-
-
-# STEP 6 - remove temporary files
+# STEP 5 - remove temporary files
 
 rm -rf $wdir
 
