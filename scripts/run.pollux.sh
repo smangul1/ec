82bf7832c5c4b415de0ade70b25b54cafca93b88
Igor <mandric.igor@gmail.com>
2017-08-27 19:37:01 -0400

    added logging and time tracking

--- scripts/run.pollux.sh
+++ scripts/run.pollux.sh
@@ -11,8 +11,6 @@ AUTHOR="imandric1"
 toolName="pollux"
 toolPath="/u/home/n/ngcrawfo/project-zarlab/igor/VIRAL_CORRECTION/tools/pollux/pollux"
 
-compressScript="/u/home/i/imandric/project-zarlab/ErrorCorrection/scripts/compress.py"
-
 # STEPS OF THE SCRIPT
 # 1) prepare input if necessary
 # 2) run the tool
@@ -23,9 +21,9 @@ compressScript="/u/home/i/imandric/project-zarlab/ErrorCorrection/scripts/compre
 
 
 # THE COMMAND LINE INTERFACE OF THE WRAPPER SCRIPT
-# $tool $input1 $input2 $outdir $kmers $others
-# |      mandatory part       | | extra part |
-# <---------------------------> <------------>
+# $tool $input1 $input2 $tmpdir $outdir $kmers $others
+# |      mandatory part               | | extra part |
+# <-----------------------------------> <------------>
 
 
 
@@ -33,15 +31,15 @@ compressScript="/u/home/i/imandric/project-zarlab/ErrorCorrection/scripts/compre
 if [ $# -lt 5 ]
 then
 echo "********************************************************************"
-echo "Script was written for project : Best practices for  conducting benchmarking in the most comprehensive and reproducible way"
+echo "Script was written for project : Best practices for conducting benchmarking in the most comprehensive and reproducible way"
 echo "This script was written by Igor Mandric"
 echo "********************************************************************"
 echo ""
-echo "1 <input1>  - _1.fastq "
+echo "1 <input1> - _1.fastq"
 echo "2 <input2> - _2.fastq"
-echo "3 <tmpdir - dir to save the intermediate output"
-echo "4 <outdir - dir to save the output"
-echo "5 <kmer> - kmer length"
+echo "3 <tmpdir> - dir to save the intermediate output"
+echo "4 <outdir> - dir to save the output"
+echo "5 <kmer>   - kmer length"
 echo "--------------------------------------"
 exit 1
 fi
@@ -59,7 +57,14 @@ kmer=$5
 
 
 
+# STEP 0 - create output directory if it does not exist
 
+mkdir -p $outdir
+logfile=$outdir/report.log
+
+# -----------------------------------------------------
+
+echo "START" >> $logfile
 
 # STEP 1 - prepare input if necessary (ATTENTION: TOOL SPECIFIC PART!)
 
@@ -72,10 +77,27 @@ kmer=$5
 
 # STEP 2 - run the tool (ATTENTION: TOOL SPECIFIC PART!)
 
+now="$(date)"
+printf "%s --- RUNNING %s\n" "$now" $toolName >> $logfile
+
 wdir=${tmpdir}/$toolName-$( date +%Y-%m-%d-%H-%M-%S )
 
 #run the command (in a temporary directory)
-$toolPath -i $input1 $input2 -p -o $wdir -k $kmer
+res1=$(date +%s.%N)
+$toolPath -i $input1 $input2 -p -o $wdir -k $kmer >> $logfile 2>&1 
+res2=$(date +%s.%N)
+dt=$(echo "$res2 - $res1" | bc)
+dd=$(echo "$dt/86400" | bc)
+dt2=$(echo "$dt-86400*$dd" | bc)
+dh=$(echo "$dt2/3600" | bc)
+dt3=$(echo "$dt2-3600*$dh" | bc)
+dm=$(echo "$dt3/60" | bc)
+ds=$(echo "$dt3-60*$dm" | bc)
+now="$(date)"
+printf "%s --- TOTAL RUNTIME: %d:%02d:%02d:%02.4f\n" "$now" $dd $dh $dm $ds >> $logfile
+
+now="$(date)"
+printf "%s --- FINISHED RUNNING %s %s\n" "$now" $toolName >> $logfile
 
 # ---------------------
 
@@ -84,9 +106,18 @@ $toolPath -i $input1 $input2 -p -o $wdir -k $kmer
 
 # STEP 3 - transform output if necessary (ATTENTION: TOOL SPECIFIC PART!)
 
-cat $wdir/input_1.fastq.corrected $wdir/input_2.fastq.corrected | gunzip - > $wdir/${toolName}.corrected.fastq.gz
+# if you need to transform fasta to fastq - here is the command:
+#     awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' file.fastq > file.fasta
+
+now="$(date)"
+printf "%s --- TRANSFORMING OUTPUT\n" "$now" >> $logfile
+
+cat $wdir/input_1.fastq.corrected $wdir/input_2.fastq.corrected | gzip > $wdir/${toolName}.corrected.fastq.gz
 rm $wdir/input_1.fastq.corrected
 rm $wdir/input_2.fastq.corrected
+
+now="$(date)"
+printf "%s --- TRANSFORMING OUTPUT DONE\n" "$now" >> $logfile
 
 # --------------------------------------
 
@@ -96,7 +127,13 @@ rm $wdir/input_2.fastq.corrected
 
 # STEP 4 - send the output to the designated directory
 
-cp $wdir/${toolName}.corrected.compressed.fastq.gz $outdir
+now="$(date)"
+printf "%s --- COPYING OUTPUT FILE\n" "$now" >> $logfile
+
+cp $wdir/${toolName}.corrected.fastq.gz $outdir
+
+now="$(date)"
+printf "%s --- COPYING OUTPUT FILE DONE\n" "$now" >> $logfile
 
 # ----------------------------------------------------
 
@@ -104,8 +141,18 @@ cp $wdir/${toolName}.corrected.compressed.fastq.gz $outdir
 
 # STEP 5 - remove temporary files
 
+now="$(date)"
+printf "%s --- REMOVING TEMPORARY FILES\n" "$now" >> $logfile
+
 rm -rf $wdir
 
+now="$(date)"
+printf "%s --- REMOVING TEMPORARY FILES DONE\n" "$now" >> $logfile
+
 # -------------------------------
+
+
+printf "DONE" >> $logfile
+
 
 
